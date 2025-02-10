e02c822060faa4c28c3fd2c55e731d584701daaf
peter safwat <petersafwat640@gmail.com>
2025-02-10 12:11:52 +0200

    added backend link

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    FRONTEND_SERVER: "http://localhost:3000",
-    BACKEND_SERVER: "http://localhost:8000/api",
-    STATIC_SERVER: "http://localhost:8000",
+    // FRONTEND_SERVER: "http://localhost:3000",
+    // BACKEND_SERVER: "http://localhost:8000/api",
+    // STATIC_SERVER: "http://localhost:8000",
 
-    // FRONTEND_SERVER: "https://ajsports.ch",
-    // BACKEND_SERVER: "https://ajsports.ch/api",
-    // STATIC_SERVER: "https://ajsports.ch",
+    FRONTEND_SERVER: "https://trading-backend-production-1d53.up.railway.app",
+    BACKEND_SERVER: "https://trading-backend-production-1d53.up.railway.app/api",
+    STATIC_SERVER: "https://trading-backend-production-1d53.up.railway.app",
   },
   images: {
     domains: ["localhost"],
