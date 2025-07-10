5f3d04dc753ba520e324193cb435617e7f8bfcba
peter safwat <petersafwat640@gmail.com>
2025-07-10 08:44:46 +0300

    removed the local env

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,9 @@ const nextConfig = {
     //   "https://trading-backend-production-9c14.up.railway.app/api",
     // NEXT_PUBLIC_STATIC_SERVER:
     //   "https://trading-backend-production-9c14.up.railway.app",
-    NEXT_PUBLIC_BACKEND_SERVER:
-      "http://localhost:8000/api",
-    NEXT_PUBLIC_STATIC_SERVER: "http://localhost:8000",
+    // NEXT_PUBLIC_BACKEND_SERVER:
+    //   "http://localhost:8000/api",
+    // NEXT_PUBLIC_STATIC_SERVER: "http://localhost:8000",
   },
   images: {
     domains: ["localhost"],
