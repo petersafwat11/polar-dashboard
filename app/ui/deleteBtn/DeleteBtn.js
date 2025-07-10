867962d10f2e9a0f53d1cddd8a338e3a0b0cc03b
peter safwat <petersafwat640@gmail.com>
2025-07-10 14:25:00 +0300

    updated the backend link

--- app/ui/deleteBtn/DeleteBtn.js
+++ app/ui/deleteBtn/DeleteBtn.js
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./delete.module.css";
 import { inter } from "@/app/fonts";
 const DeleteBtn = ({ deletepage }) => {
+  //sss
   return (
     <button className={`${inter.className} ${classes["delete-btn"]}`} onClick={deletepage}>
       Delete
